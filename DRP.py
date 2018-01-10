59cdbb74648b4ef6d98482116c8c11bc64b897dd
Ankit Agarwal <aagarwal_601@berkeley.edu>
2018-01-10 11:11:28 -0600

    Added changes to research and added folder for other project
    Added changes to research and added folder for other project

--- DRP.py
+++ DRP.py
@@ -15,9 +15,10 @@ class Dihedral_Element():
             new_num_ab  = (elem1.num_ab + elem2.num_ab) % elem1.order
         else:
             new_num_ab = (elem1.num_ab - elem2.num_ab) % elem2.order
+        return Dihedral_Element(new_num_ab, elem1.order)
 
     def __str__(self):
-        return 'd' + self.num_ab  
+        return 'd' + str(self.num_ab % self.order)  
         return Dihedral_Element(new_num_ab, order)
     def __eq__(elem1, elem2):
         assert elem1.order == elem2.order
@@ -34,10 +35,10 @@ class Z_mod_n():
         assert elem1.order == elem2.order
         return Z_mod_n((elem1.number + elem2.number) % elem1.order, elem1.order)
     def __str__(self):
-        return self.number
+        return 'z' + str(self.number)
     def __eq__(elem1, elem2):
         assert elem1.order == elem2.order
-        if elem1.number = elem2.number:
+        if elem1.number == elem2.number:
             return True
         return False
 class Z_cross_D():
@@ -48,7 +49,7 @@ class Z_cross_D():
     def __mul__(elem1, elem2):
         return Z_cross_D(elem1.dihedral * elem2.dihedral, elem1.integer * elem2.integer)
 
-    def __str__(str):
+    def __str__(self):
         return '(' + str(self.dihedral) + ", " + str(self.integer) + ")"
     def __eq__(elem1, elem2):
         return elem1.dihedral == elem2.dihedral and elem2.integer == elem1.integer
@@ -71,7 +72,7 @@ class Group_Ring_Element():
                     coefs[index] = (coefs[index] + coefs[index2]) % modulo
                     remove_list.append(index2)
                     index2 += 1
-            if coefs[index] = 0:
+            if coefs[index] == 0:
                 remove_list.add(index)
                 index -= 1
             [coefs.pop(i) for i in remove_list]
@@ -81,11 +82,31 @@ class Group_Ring_Element():
         return (elements, coefs)
         
     def __mul__(elem1, elem2):
-        
+        assert elem1.isInstance(Group_Ring_Element)
+        assert elem2.isInstance(Group_Ring_Element)
+        assert elem1.modulo == elem2.modulo
+        coeff_m = []
+        elem_m = []
+        for index1 in range(len(elem1.group_elems)):
+            for index2 in range(len(elem2.group_elems)):
+                coef_m.append(mul(elem1.coefs[index1], elem2.coefs[index2]))
+                elem_m.append(mul(elem1.elements[index1], elem2.elements[index2]))
+        return Group_Ring_Element(elem_m, coef_m, elem1.modulo)
 
 
+    def __str__(self):
+        str_rep = ''
+        for i in range(len(self.elements)):
+            if i < len(self.elements) - 1:
+                str_rep += str(self.coefs[i]) + " * " + str(self.elements[i]) + " + ";
+            else:
+                str_rep += str(self.coefs[i]) + " * " + str(self.elements[i])
+        return str_rep     
 
+def create_identity_d(n):
+    return Dihedral_Element(0, n)
 
-
-        
-
+def create_identity_z(n):
+    return Z_mod_n(0, n)
+def create_identity_z_cross_d(n):
+    return Z_cross_D(create_identity_d(n), create_identity_z(n))
